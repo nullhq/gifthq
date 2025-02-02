8c4827d261c9531feafb62b56e3dd75f6492ec86
taboutsafredy <taboutsafredy@gmail.com>
2025-02-02 12:05:41 +0100

    feat(ui): restructuring and adding basic styles

---
+++ giftUI/src/App.tsx
@@ -0,0 +1,13 @@
+// Path: src/App.tsx
+// App container where w'll wrap all our components.
+
+import "./App.css";
+
+function App () {
+    return (
+        <>
+        </>
+    );
+}
+
+export default App;
