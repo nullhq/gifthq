bf647826f42e0fb177f7026536ae056731a39019
taboutsafredy <taboutsafredy@gmail.com>
2025-02-10 13:54:57 +0100

    feat(ui/components): take in count the case that we're on Frens tab

--- giftUI/src/components/Empty/Empty.tsx
+++ giftUI/src/components/Empty/Empty.tsx
@@ -7,17 +7,22 @@ import Lottie from "../Lottie/Lottie";
 interface IEmpty {
     message: string;
     goto: string;
+    inFrens?: boolean;
 }
 
-function Empty ({ message, goto }: IEmpty) {
+function Empty ({ message, goto, inFrens }: IEmpty) {
     const gift = new URL("../../assets/lotties/tabs/tab-gifts.json", import.meta.url).href;
+    const frens = new URL("../../assets/lotties/empty/frens.lottie", import.meta.url).href;
 
     return (
         <div className={styles.empty}>
-            <Lottie path={gift} isActive={true} autoplay={true} style={{
+            { inFrens ? <Lottie path={frens} isActive={true} autoplay={true} style={{
                 width: "auto",
                 height: "80px"
-            }} />
+            }} /> : <Lottie path={gift} isActive={true} autoplay={true} style={{
+                width: "auto",
+                height: "80px"
+            }} /> }
             <p className={styles.empty_message}>{message}</p>
             <p className={styles.empty_go_to}>{goto}</p>
         </div>
