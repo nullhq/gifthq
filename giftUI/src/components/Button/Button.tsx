d6568d686a2d5cb761882f2a75b217ff6618db8a
taboutsafredy <taboutsafredy@gmail.com>
2025-02-10 13:46:20 +0100

    feat(components): upgrade Button in accordance with our Earn page

--- giftUI/src/components/Button/Button.tsx
+++ giftUI/src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 // Path: src/components/Button/Button.tsx
-// Our custom button component who w'll be use instead of the native Telegram main button when app is open in a browser.
+// Our custom button component that w'll be use instead of the native Telegram main button when app is open in a browser.
 
 import styles from "./Button.module.css";
 
@@ -8,11 +8,12 @@ interface IButton {
     onClick?: () => void;
     labelSecondary?: string;
     onClickSecondary?: () => void;
+    earn?: boolean;
 }
 
-function Button ({ label, onClick, labelSecondary, onClickSecondary }: IButton) {
+function Button ({ label, onClick, labelSecondary, onClickSecondary, earn }: IButton) {
     return (
-        <div className={styles.custom_button_ctn}>
+        <div className={`${styles.custom_button_ctn} ${earn ? styles.earn : ''}`}>            
             <button className={styles.button} onClick={onClick}>
                 {label}
             </button>
